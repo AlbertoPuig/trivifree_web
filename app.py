69db15e75917ff2370471b942e6e83b1eba27e2b
AlbertoPuig <alberto.puig.rguez@gmail.com>
2018-02-03 08:33:21 +0100

    add score and clean code

--- app.py
+++ app.py
@@ -23,37 +23,25 @@ def get_question(question_id):
 
 	try:
 		url = load_properties('local','url')
-		#url = 'http://127.0.0.1:5000/trivifree/api/v1/quest/1'
 		response = requests.get(url + str(question_id))
-		#response = requests.get(url)
-		print "Antes de llamar a la api"
+
 		if response.status_code == 200:
 			jData = response.json()
-			print "Devuelve un 200_____________-"
-			print jData
 			return jData
 		else:
 			return None
 	except Exception as ex:
 		print "Error_____________________: " + str(ex)
 
-
-
-
+#main route load template
 @app.route("/")
 def main():
-    #return "Welcome!"
-    #return render_template('index.html', option1 = 'pepe', option2 = 'juan', option3 = 'pedro', question = 'Who is?')
     return render_template('index.html')
-
+#route with param. Call backend api
 @app.route('/ajax/<question_id>', methods = ['POST'])
 def ajax_request(question_id):
-	#username = request.form['username']
 	jsondata = get_question(question_id)
-	print "Retornado el valor a /ajax__________________________--"
 	return json.dumps(jsondata)
-	#return json.dumps({'option1':'OPTION_1','option2': 'OPTION_2','option3': 'OPTION_3'});
-
 
 if __name__ == "__main__":
     app.run(port="5001")
